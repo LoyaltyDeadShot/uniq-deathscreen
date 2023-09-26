ce6270584e16859c2c9cc04a4bfdc793f089512e
Katoteki <andiglava2001@gmail.com>
2023-09-26 14:05:26 +0200

    drag and drop

--- client/client.lua
+++ client/client.lua
@@ -1,45 +1,58 @@
-RegisterNetEvent('uniq-deathscreen:client:onPlayerDeath')
-AddEventHandler('uniq-deathscreen:client:onPlayerDeath', function(isDead)
-    if isDead then
-        local killername = nil
-        TriggerEvent('uniq-deathscreen:client:setDeathStatus', true)
-        SendNUIMessage({type = "show", status = true})
-        SetNuiFocus(true, true)
+local function onPlayerDeath()
+    local killername = nil
+    TriggerEvent('uniq-deathscreen:client:setDeathStatus', true)
+    SendNUIMessage({type = "show", status = true})
+    SetNuiFocus(true, true)
 
-        local PedKiller = GetPedSourceOfDeath(PlayerPedId())
-        local killerid = NetworkGetPlayerIndexFromPed(PedKiller)
+    local PedKiller = GetPedSourceOfDeath(PlayerPedId())
+    local killerid = NetworkGetPlayerIndexFromPed(PedKiller)
 
-        if IsEntityAVehicle(PedKiller) and IsEntityAPed(GetVehiclePedIsIn(PedKiller, -1)) and IsPedAPlayer(GetPedInVehicleSeat(PedKiller, -1)) then
-            killerid = NetworkGetPlayerIndexFromPed(GetPedInVehicleSeat(PedKiller, -1))
-        end
-
-        if (killerid == -1) then
-            killername = Config.Translation.Suicide
-        elseif (killerid == nil) then
-            killername = Config.Translation.Unknown
-        elseif (killerid ~= -1) then
-
-            if Config.UseRPName then
-                TriggerServerEvent('uniq-deathscreen:server:getRPName', GetPlayerServerId(killerid))
-            else
-                killername = GetPlayerName(killerid)
-            end
-        end
-
-        SendNUIMessage({type = 'setUPValues', killer = killername, timer = Config.Timer})
+    if IsEntityAVehicle(PedKiller) and IsEntityAPed(GetVehiclePedIsIn(PedKiller, -1)) and IsPedAPlayer(GetPedInVehicleSeat(PedKiller, -1)) then
+        killerid = NetworkGetPlayerIndexFromPed(GetPedInVehicleSeat(PedKiller, -1))
     end
-end)
+
+    if (killerid == -1) then
+        killername = Config.Translation.Suicide
+    elseif (killerid == nil) then
+        killername = Config.Translation.Unknown
+    elseif (killerid ~= -1) then
+
+        if Config.UseRPName then
+            TriggerServerEvent('uniq-deathscreen:server:getRPName', GetPlayerServerId(killerid))
+        else
+            killername = GetPlayerName(killerid)
+        end
+    end
+
+    SendNUIMessage({type = 'setUPValues', killer = killername, timer = Config.Timer})
+end
+
+RegisterNetEvent('uniq-deathscreen:client:onPlayerDeath', onPlayerDeath)
 
 RegisterNetEvent('uniq-deathscreen:client:getRPName')
 AddEventHandler('uniq-deathscreen:client:getRPName', function(name)
     SendNUIMessage({type = 'setUPValues', killer = name, timer = Config.Timer})
 end)
 
-RegisterNetEvent('uniq-deathscreen:client:hide_ui')
-AddEventHandler('uniq-deathscreen:client:hide_ui', function()
+local function HideUI()
     SendNUIMessage({type = "show", status = false})
     SetNuiFocus(false, false)
-end)
+end
+
+RegisterNetEvent('uniq-deathscreen:client:hide_ui', HideUI)
+
+if GetResourceState('es_extended'):find('start') then
+    AddEventHandler('esx:onPlayerDeath', onPlayerDeath)
+    AddEventHandler('esx:onPlayerSpawn', HideUI)
+elseif GetResourceState('qb-core'):find('start') then
+    RegisterNetEvent('QBCore:Player:SetPlayerData', function(data)
+        if (data.metadata.isdead or data.metadata.inlaststand) then
+            onPlayerDeath()
+        else
+            HideUI()
+        end
+    end)
+end
 
 RegisterNuiCallback("accept_to_die", function(data)
     SendNUIMessage({type = "show", status = false})
